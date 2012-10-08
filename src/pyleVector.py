daa3c494c37e8115411486a4973b6830d62c14f7
Daniel Sanchez Quiros <3danimanimal@gmail.com>
2012-10-08 17:20:03 +0200

    src folder, pyleVector.py file, added

---
+++ src/pyleVector.py
@@ -0,0 +1,35 @@
+import pymel.core as pm
+import OpenMaya as om
+
+
+
+
+class Pyletool:
+    """
+    Tool thtat takes 3 selected bones with an ik solver 
+    and placed a locator where the polevector imfluent object must be.
+    """
+    def __init__(self):
+        """Initialize data, including basic UI"""
+        pass
+    def retrieve(self):
+        """Retrieve data from the selected objects"""
+        pass
+    def doit(self):
+        """Place a locator over the correct coordinates"""
+        pass
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
