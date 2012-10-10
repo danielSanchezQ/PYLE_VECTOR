88bf7dba6df2fe4f6c515007a0bdbeefe0e0a891
Daniel Sanchez Quiros <3danimanimal@gmail.com>
2012-10-10 18:37:35 +0200

    main code added and working

--- src/pyleVector.py
+++ src/pyleVector.py
@@ -14,9 +14,37 @@ class Pyletool:
         pass
     def retrieve(self):
         """Retrieve data from the selected objects"""
-        pass
+        selection = pm.ls(sl=True, type ='joint')
+        if not len(selection) == 3:
+            raise ValueError("Selection must be of lengh 3")
+        else:
+            self.objects = selection
+    def calcPos(self):
+        if not self.objects:
+            raise AttributeError("None objects have been selected")
+        else:
+            #Querry positions
+            dataA = pm.xform(self.objects[0],q=True,ws=True,t=True)
+            dataB = pm.xform(self.objects[1],q=True,ws=True,t=True)
+            dataC = pm.xform(self.objects[2],q=True,ws=True,t=True)
+            #Inicializate joints vectors throug position querry
+            vA = om.MVector(dataA[0],dataA[1],dataA[2])
+            vB = om.MVector(dataB[0],dataB[1],dataB[2])
+            vC = om.MVector(dataC[0],dataC[1],dataC[2])
+            #Vector calculation
+            size = vC - vA
+            halfsize = size * .5
+            halfpos = vA + halfsize
+            returnpos = (vB + halfpos) * 2
+            return returnpos
+            
     def doit(self):
         """Place a locator over the correct coordinates"""
+        self.retrieve()
+        pos = self.calcPos()
+        self.loc = pm.spaceLocator
+        pm.setAttr(self.loc+'.translate', pos)
+
         pass
     
     
